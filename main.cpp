a334748200638f94257108469f1f22d3a63829af
GennadijBratchikovIT14 <newungitstudy@gmail.com>
2024-12-03 10:33:05 +0500

    Сделал генерацию рандомного числа в строку, без повторения цифр

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
+string generateSecretNumber() {
+    string secret = "";
+    srand(time(NULL));
+    int A[4] = {};
+    int count = 0;
+
+    while (count < 4) {
+        int temp = rand() % 10;
+        bool unique = true;
+
+
+        for (int j = 0; j < count; j++) {
+            if (A[j] == temp) {
+                unique = false;
+                break;
+            }
+        }
+
+        if (unique) {
+            A[count] = temp;
+            secret += (char) (temp + '0');
+            count++;
+        }
+    }
+
+    return secret;
+}
+
+
+int main() {
+    string secret = generateSecretNumber();
+    cout << secret;
+}
