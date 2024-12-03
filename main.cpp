3e61be46a02d2c27db616ca6eb9d423c287c7478
GennadijBratchikovIT14 <newungitstudy@gmail.com>
2024-12-03 11:07:36 +0500

    Полностью рабочий код программы, но нужно сделать оформление

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ string generateSecretNumber() {
         }
 
         if (unique) {
+
             A[count] = temp;
             secret += (char) (temp + '0');
             count++;
@@ -33,7 +34,56 @@ string generateSecretNumber() {
 }
 
 
+
+int countBullsAndCows(const string& secret, const string& guess) {
+    int bulls = 0, cows = 0;
+    bool secretUsed[4] = {false};
+
+
+    for (int i = 0; i < guess.length(); i++) {
+        if (guess[i] == secret[i]) {
+            bulls++;
+            secretUsed[i] = true;
+        }
+    }
+
+
+    for (int i = 0; i < guess.length(); i++) {
+        if (guess[i] != secret[i]) {
+            for (int j = 0; j < secret.length(); j++) {
+                if (!secretUsed[j] and guess[i] == secret[j]) {
+                    cows++;
+                    secretUsed[j] = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    return bulls * 10 + cows;
+}
+
+
+bool isDigits(const string& str) {
+    for (char i : str) {
+        if(!isdigit(i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
 int main() {
     string secret = generateSecretNumber();
-    cout << secret;
+    cout << secret << endl;
+    string guess;
+
+    while (true) {
+        cin >> guess;
+        int result = countBullsAndCows(secret, guess);
+        cout << result << endl;
+    }
 }
