88268ab1ce0644af082db66ebc3f37f6b2412d80
Gennadij Bratchikov IT14 <newungitstudy@gmail.com>
2024-12-10 10:26:28 +0500

    Update header.h

--- header.h
+++ header.h
@@ -1,7 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <iostream>
 #include <string>
+#include <ctime>
+#include <iomanip>
+#include "header.h"
 
 string generateSecretNumber();
 int countBullsAndCows(const string& secret, const string& guess);
